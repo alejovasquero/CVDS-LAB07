1f720e9d9e23a52db08b80ab883c4a35043f4f6c
Silenrate <daniwalteros596@gmail.com>
2019-10-08 10:58:16 -0500

    RegistroClientes

--- src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
+++ src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -1,3 +1,42 @@
 package edu.eci.cvds.view;
 
-public class AlquilerItemsBean{}
+import com.google.inject.Inject;
+import edu.eci.cvds.samples.entities.Cliente;
+import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.cvds.samples.services.ServiciosAlquiler;
+
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+
+@ManagedBean(name = "alquilerBean")
+public class AlquilerItemsBean extends BasePageBean{
+
+    private Cliente selectedCliente;
+    @Inject
+    private ServiciosAlquiler serviciosAlquiler;
+
+    public List<Cliente> consultarClientes(){
+        List<Cliente> clientes = null;
+        try {
+            clientes = serviciosAlquiler.consultarClientes();
+        } catch (ExcepcionServiciosAlquiler excepcionServiciosAlquiler) {
+        }
+        return clientes;
+    }
+
+    public void agregarCliente(String nombre, long documento, String telefono, String direccion, String email){
+        try {
+            serviciosAlquiler.registrarCliente(new Cliente(nombre,documento,telefono,direccion,email));
+        } catch (ExcepcionServiciosAlquiler excepcionServiciosAlquiler) {
+
+        }
+    }
+
+    public Cliente getSelectedCliente(){
+        return selectedCliente;
+    }
+
+    public void setSelectedCliente(Cliente selectedCliente){
+        this.selectedCliente = selectedCliente;
+    }
+}
