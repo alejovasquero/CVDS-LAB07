05b8a8df0cdbda42eb7f1154e5cde775cdb94a25
Silenrate <daniwalteros596@gmail.com>
2019-10-08 11:40:35 -0500

    Registro Finalizado

--- src/main/java/edu/eci/cvds/samples/services/client/Main.java
+++ src/main/java/edu/eci/cvds/samples/services/client/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws ExcepcionServiciosAlquiler {
         System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarCliente(123456));
+        System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarClientes());
         System.exit(0);
     }
 }
