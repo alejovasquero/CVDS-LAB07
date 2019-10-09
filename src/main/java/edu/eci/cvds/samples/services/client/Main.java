01f807983cfc91d15f5c225947fefe1fb8525190
alejovasquero <alejo_704@hotmail.com>
2019-10-09 12:55:16 -0500

    ARREGLO

--- src/main/java/edu/eci/cvds/samples/services/client/Main.java
+++ src/main/java/edu/eci/cvds/samples/services/client/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws ExcepcionServiciosAlquiler {
         System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarCliente(123456));
-        System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarClientes());
+        System.out.println(ServiciosAlquilerFactory.getInstance().getServiciosAlquiler().consultarItemsRentadosSinDevolver());
         System.exit(0);
     }
 }
